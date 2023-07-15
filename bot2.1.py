3ee1afb9459c14a2d8ce81816dfd6c71f2edbe63
harshit-20cse <harshit.20cse@cambridge.edu.in>
2023-07-15 14:45:05 +0530

    Basic changes

--- bot2.1.py
+++ bot2.1.py
@@ -169,7 +169,6 @@ def index():
     # return "Post done successfully!"
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    pushDataInTheFirebase()
+    app.run()
 
-pushDataInTheFirebase()
